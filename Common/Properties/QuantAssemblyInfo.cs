97f2f60c6cc2debfa10ea0c84715ddeb5a875a54
Vladislav Dyrov <me@xtnd8.com>
2020-04-28 20:56:07 +0300

    quant facets restored

---
+++ Common/Properties/QuantAssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyProduct("Exomarkt Quant")]
+[assembly: AssemblyTitle("Excieso.Exomarkt.Quant")]
+
+[assembly: InternalsVisibleTo("Excieso.Exomarkt.Quant.Algorithm.Framework")]
+[assembly: InternalsVisibleTo("Excieso.Exomarkt.Quant.Brokerages")]
+[assembly: InternalsVisibleTo("Excieso.Exomarkt.Quant.Engine")]
+[assembly: InternalsVisibleTo("Excieso.Exomarkt.Quant.Tests")]
